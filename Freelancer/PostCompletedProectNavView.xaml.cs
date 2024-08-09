71ed996cd96ca7d8cb5fddb610ffea784fc9b6b9
Harry Kachigamba <152857607+Harry-Kachigamba@users.noreply.github.com>
2024-08-09 23:20:35 +0200

    Update PostCompletedProectNavView.xaml.cs

--- Freelancer/PostCompletedProectNavView.xaml.cs
+++ Freelancer/PostCompletedProectNavView.xaml.cs
@@ -37,7 +37,7 @@ namespace Freelance_Platform_Final.Freelancer
                 }
 
                 // Connect to MySQL database
-                string connectionString = "server=localhost; port=3307; user id=root; password=12345; database=Freelance";
+                string connectionString = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
                 using var connection = new MySqlConnection(connectionString);
                 await connection.OpenAsync();
 
